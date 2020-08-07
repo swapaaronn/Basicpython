2210c183b2cd97e731addf0f9fb9814e9d826d21
swapnil <swapnilmali005@gmail.com>
2020-08-07 19:27:28 +0530

    init

--- sumofnumberwhile&for_loop.py
+++ sumofnumberwhile&for_loop.py
@@ -8,14 +8,14 @@ for i in range(x,y):
      
      
 example=(input("enter some value"))
-#y=int(input("enter last value"))
+#y=int(input('enter last value'))
 total=0
 i=0
 while i<=len(example):
     total=total+int(example[i])
     i=i+1
     print(total)
-    
+
 '''name=input("enter name")
 total=0
 for i in name:
